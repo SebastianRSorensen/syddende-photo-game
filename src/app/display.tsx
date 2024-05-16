da2797941c1f8734bf3e5e56bf4dc975bf839358
Martin Øvsttun <marovsttun@gmail.com>
2024-05-16 19:18:24 +0200

    feat: load images from public folder, remove b64 enc

--- src/app/display.tsx
+++ src/app/display.tsx
@@ -1,56 +1,56 @@
 "use client";
-import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
+import {useEffect, useState} from "react";
+import {Button} from "@/components/ui/button";
 import Image from "next/image";
 
-export function Display({ shuffledArray }: { shuffledArray: any[] }) {
-  const [index, setIndex] = useState(0);
-  const [isClient, setIsClient] = useState(false);
-  const [reveal, setReveal] = useState(true);
+export function Display({shuffledArray}: { shuffledArray: any[] }) {
+    const [index, setIndex] = useState(0);
+    const [isClient, setIsClient] = useState(false);
+    const [reveal, setReveal] = useState(true);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
-  if (index > shuffledArray.length - 1) {
-    return <div>End</div>;
-  }
+    if (index > shuffledArray.length - 1) {
+        return <div>End</div>;
+    }
 
-  const handleOnClick = () => {
-    setIndex(index + 1);
-    setReveal(true);
-  };
+    const handleOnClick = () => {
+        setIndex(index + 1);
+        setReveal(true);
+    };
 
-  const handleOnReveal = () => {
-    setReveal(false);
-  };
+    const handleOnReveal = () => {
+        setReveal(false);
+    };
 
-  if (!isClient) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div className="grid grid-cols-1 gap-5 p-10">
-      <div className="flex flex-row justify-center">
-        <Image
-          src={shuffledArray[index].photo}
-          className="object-cover rounded-lg shadow-lg"
-          alt={`Photo ${index}`}
-          width={800}
-          height={800}
-        />
-      </div>
-      <div className="flex flex-row justify-center gap-1  ">
-        {reveal ? (
-          <Button onClick={handleOnReveal}>Reveal</Button>
-        ) : (
-          <div className="flex flex-col gap-4">
-            <Button onClick={handleOnClick}>Next</Button>
-            <span className="text-2xl font-bold">
+    if (!isClient) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div className="grid grid-cols-1 gap-5 p-10">
+            <div className="flex flex-row justify-center">
+                <Image
+                    src={shuffledArray[index].uri}
+                    className="object-cover rounded-lg shadow-lg"
+                    alt={`Photo ${index}`}
+                    width={500}
+                    height={500}
+                />
+            </div>
+            <div className="flex flex-row justify-center gap-1  ">
+                {reveal ? (
+                    <Button onClick={handleOnReveal}>Reveal</Button>
+                ) : (
+                    <div className="flex flex-col gap-4">
+                        <Button onClick={handleOnClick}>Next</Button>
+                        <span className="text-2xl font-bold">
               {shuffledArray[index].owner}
             </span>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
